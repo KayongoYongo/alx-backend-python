fe452ce37f8f70788511c5bb5163a0fd9942724f
KayongoYongo <sknsammie10@gmail.com>
2023-07-06 17:34:53 +0000

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
-#!/usr/bin/bin python3
+#!/usr/bin/env python3
 """This function finds the floor of a number"""
 import math
 
 
 def floor(n: float) -> int:
-    """FInds the floor of a number"""
+    """Finds the floor of a number"""
     return math.floor(n)
