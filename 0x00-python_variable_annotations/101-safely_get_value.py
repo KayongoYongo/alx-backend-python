b91f0c347257e714770edf78fe40b5c401f0134b
KayongoYongo <sknsammie10@gmail.com>
2023-07-06 22:02:59 +0000

    get values safely

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,13 +2,19 @@
 """Get value safely"""
 
 
-from typing import TypeVar, Dict, Optional
+from typing import Mapping, TypeVar, Union, Any
 
 
 K = TypeVar('K')
 V = TypeVar('V')
 
-def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> Optional[V]:
+
+def safely_get_value(
+    dct: Mapping[K, V],
+    key: Any,
+    default: Union[V, None] = None
+) -> Union[Any, V]:
+
     """Get value safely"""
     if key in dct:
         return dct[key]
