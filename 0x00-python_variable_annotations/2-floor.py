60e76608a6c65912dc461d8ea1b8ce9800ee1faf
KayongoYongo <sknsammie10@gmail.com>
2023-07-06 17:31:07 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bin python3
+"""This function finds the floor of a number"""
+import math
+
+
+def floor(n: float) -> int:
+    """FInds the floor of a number"""
+    return math.floor(n)
