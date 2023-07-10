c8fb6171b4a26921289cdb1c5ad2a1a3f4e378f1
KayongoYongo <sknsammie10@gmail.com>
2023-07-10 18:09:07 +0000

    random module with async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """The function waits for a random delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
