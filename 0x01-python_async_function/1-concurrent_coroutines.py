e2cf7d84a9297ef2d79415d0d68f9f7530c2f03d
KayongoYongo <sknsammie10@gmail.com>
2023-07-10 19:02:53 +0000

    random lst in async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ from heapq import nsmallest
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrent execution"""
     random_list = []
