a2a62eb7947667c5544f89b928e17bcba28c4178
KayongoYongo <sknsammie10@gmail.com>
2023-07-31 18:04:32 +0000

    unit_test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+"""Unit test for utils"""
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
 
